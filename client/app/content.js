094b2e9bafff1baa9fc42e245d9f6d5110421aca
Julio Silva <julio.sguz@gmail.com>
2017-01-02 09:55:50 -0400

    v0.3.1 - New proposal for home page - Logo added - New fonts added

--- client/app/content.js
+++ client/app/content.js
@@ -1,3 +1,51 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	
+document.addEventListener('DOMContentLoaded', function(event) {
+  function controlClassForBar(state) {
+    
+    function getClasses(element) {
+      var elementClasses = document.getElementById(element).className;
+
+      if( elementClasses.length === 0 ) {
+        return false;
+      }
+
+      return elementClasses.split(' ');    
+    }
+
+    function checkHeaderClass( headerModifier, headerClass) {
+      var classes = getClasses(headerClass);
+
+      if ( classes !== false ) {
+        return classes.find(function(item) {
+          return item === headerModifier;
+        });
+      } else {
+        return false
+      }
+    }
+
+    var headerModifier = 'header--blue',
+      headerClass = 'header';
+
+    if ( document.body.scrollTop > 90 ) {     
+
+      if( !checkHeaderClass(headerModifier, headerClass) ) {
+        document.getElementById(headerClass).className += ' ' + headerModifier;
+      }
+
+    } else {
+      
+      if( document.body.scrollTop === 0 ) {
+        
+        if( checkHeaderClass(headerModifier, headerClass) ) {
+          var classes = getClasses(headerClass);
+          document.getElementById(headerClass).className = classes.filter(function(item) {
+            return item !== headerModifier;
+          }).join(' ');
+        }
+      }
+    }
+  }
+
+  window.addEventListener('scroll', controlClassForBar);
+
 });
